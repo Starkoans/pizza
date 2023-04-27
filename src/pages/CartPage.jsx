a4f0f219e9af792697f676fef324841e49fff531
4g-eater <tasya16mosya@gmail.com>
2023-04-27 22:52:04 +0700

    clean imports

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,4 @@
-import {Link} from "react-router-dom";
-import {CartItems} from "../DB/CartItems.js";
 import Cart from "../components/Cart.jsx";
-import {useContext, useEffect} from "react";
 import {useSelector} from "react-redux";
 import styles from './styles/CartPage.module.css'
 
